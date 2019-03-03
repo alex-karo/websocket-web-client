299d7eb4c51d30d8392dcfdade2f3c7ae32ddda8
Alex Karo <alex@karo-dev.ru>
2019-03-03 20:53:47 +0300

    Updates

---
+++ precache-manifest.9ff4f38ade1651619246495c8aff33a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74f71385d2f8300f0c4d528745763f65",
+    "url": "/websocket-web-client/static/media/github.74f71385.svg"
+  },
+  {
+    "revision": "5039b923c5b3c74a76a6",
+    "url": "/websocket-web-client/static/js/runtime~main.5039b923.js"
+  },
+  {
+    "revision": "fc4305d105f2228ea9f4",
+    "url": "/websocket-web-client/static/js/main.fc4305d1.chunk.js"
+  },
+  {
+    "revision": "c7bfabb685b46874511a",
+    "url": "/websocket-web-client/static/js/2.c7bfabb6.chunk.js"
+  },
+  {
+    "revision": "e81df7710c39be5ac79420c059ab30e4",
+    "url": "/websocket-web-client/index.html"
+  }
+];
